c8e95916e1e734957ba36b58e27715d485f698ae
chenwanan@exchain.com <imeahu@163.com>
2019-03-27 18:29:52 +0800

    no message

--- test/account.test.js
+++ test/account.test.js
@@ -2,4 +2,6 @@ const Bumo = require('../lib/sdk.js');
 const b = new Bumo({
     host: 'seed1.bumotest.io:26002',
 });
-console.log(b.account.create());
+let account = b.account.create();
+console.log(b.account.checkValid(account.result.address));
+console.log(b.account.getInfo(account.result.address));
